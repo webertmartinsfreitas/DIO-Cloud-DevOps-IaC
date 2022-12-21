20113557a747982c8571cc76665036021fb8fe72
Webert Martins de Freitas <68933320+webertmartinsfreitas@users.noreply.github.com>
2022-12-21 05:11:41 -0300

    Update and rename iac1.sh to Script_IaC_Configuracao_Servidor.sh

--- Script_IaC_Configuracao_Servidor.sh
+++ Script_IaC_Configuracao_Servidor.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
 echo "Criando diretórios..."
-
 mkdir /publico
 mkdir /adm
 mkdir /ven
 mkdir /sec
 
 echo "Criando grupos de usuários..."
-
 groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
 echo "Criando usuários..."
-
 useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
@@ -28,7 +25,6 @@ useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 
 echo "Especificando permissões dos diretórios...."
-
 chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
 chown root:GRP_SEC /sec

27bba4f23f71532072456ec8fd10c0c54a7228e1
Webert Martins de Freitas <68933320+webertmartinsfreitas@users.noreply.github.com>
2022-12-21 05:07:25 -0300

    Update script-iac2.sh

--- script-iac2.sh
+++ script-iac2.sh
@@ -6,14 +6,9 @@ apt-get upgrade -y
 apt-get install apache2 -y
 apt-get install unzip -y
 
-
 echo "Baixando e copiando os arquivos da aplicação..."
-
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
 cd linux-site-dio-main
 cp -R * /var/www/html/
-
-
-
